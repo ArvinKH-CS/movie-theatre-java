fab90aeef0dc220362a3383461c7c80724483156
ArvinKH-CS <73275285+ArvinKH-CS@users.noreply.github.com>
2021-05-01 13:56:05 +0400

    Updated java list
    Added an option to choose which movie you'd like to view.

--- src/list.java
+++ src/list.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class list {
 
@@ -61,21 +62,59 @@ public class list {
         price = i2;
     }
 
-    list knivesOut = new list("Knives Out", "Rian Johnson",
+    public String toString() {
+        return "The movie " + title + " is directed by " + director + ", and stars " + cast +
+                ". It was released in " + year + ", and has a price of " + price + " per ticket.";
+    }
+
+    static list knivesOut = new list("Knives Out", "Rian Johnson",
             Arrays.asList("Daniel Craig", "Chris Evans", "Ana de Armas"), 2019, 4.99f);
 
-    list tenet = new list("Tenet", "Christopher Nolan",
+    static list tenet = new list("Tenet", "Christopher Nolan",
             Arrays.asList("John David Washington", "Robert Pattinson", "Elizabeth Debicki"), 2020, 6.99f);
 
-    list joker = new list("Joker", "Todd Phillips",
+    static list joker = new list("Joker", "Todd Phillips",
             Arrays.asList("Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"), 2019, 3.99f);
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
-    List<list> movies = new ArrayList<>();
-    movies.add(knivesOut);
-    movies.add(tenet);
-    movies.add(joker);
+        List<list> movies = new ArrayList<>();
+        movies.add(knivesOut);
+        movies.add(tenet);
+        movies.add(joker);
+
+            for(int i = 0; i < movies.size(); i++) {
+                System.out.println("---------------------------------");
+                System.out.println(i+1 + ". " + movies.get(i).toString());
+            }
+
+            System.out.println("Please choose the movie you'd like to view.");
+
+            Scanner userChoice = new Scanner(System.in);
+            int movieChoice = userChoice.nextInt();
+
+            System.out.println("Your choice is: " + movies.get(movieChoice-1).toString());
+            System.out.println("Is that correct?");
+
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+
+            int rightWrong = userChoice.nextInt();
+
+                while(rightWrong == 2){
+                    System.out.println("Please choose the movie you'd like to view.");
+                    movieChoice = userChoice.nextInt();
+
+                    System.out.println("Your choice is: " + movies.get(movieChoice-1).toString());
+                    System.out.println("Is that correct?");
+
+                    System.out.println("1. Yes");
+                    System.out.println("2. No");
+
+                    rightWrong = userChoice.nextInt();
+                }
+
+            int chosen = movieChoice;
 
     }
 }
